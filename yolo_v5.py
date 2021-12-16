f5cd2e0ef5f90625ab986a52826048e5a3fff037
ZARIF ubuntu <uzbzarif@gmail.com>
2021-12-16 13:31:56 +0900

    val train

--- yolo_v5.py
+++ yolo_v5.py
@@ -1,22 +1,50 @@
 import fiftyone as fo
+import argparse
+# Define the function create dataset
+def get_dataset(path):
+ 
+    # Dataset name
+    name = "yolo_v5_dataset"
+    # The splits to load
+    splits = ["train", "val"]
+    dataset_dir = path
 
-name = "my-dataset"
-dataset_dir = "/media/buntuml/DATASET/DAMAGEAI/REPORT/efflorescence_1/CONCRETE_DATASET_YOLOv5"
-# dataset_dir = '/home/buntuml/Documents/dataset/North American Mushrooms.v1-416x416 (1).yolov5pytorch'
-# label = '/home/buntuml/github/damage.ai/CrackDetectionDataset/CONCRETE_DATASET_YOLOv5/dataset.yml'
-session = fo.launch_app()
-# Create the dataset
-dataset = fo.Dataset.from_dir(
-    dataset_dir=dataset_dir,
-    dataset_type=fo.types.YOLOv5Dataset,
-    # labels_path=label,
-    name=name,
-)
-print(type(dataset))
-# View summary info about the dataset
-session.dataset = dataset
-print(dataset)
-session.wait()
+    # Load the dataset, using tags to mark the samples in each split
+    dataset = fo.Dataset(name)
+    for split in splits:
+        dataset.add_dir(
+            dataset_dir=dataset_dir,
+            dataset_type=fo.types.YOLOv5Dataset,
+            split=split,
+            tags=split,
+    )
+    return dataset
 
-# Print the first few samples in the dataset
-# print(dataset.head())
+#Define the function to parse the command line arguments
+def parse_args():
+    parser = argparse.ArgumentParser(description="Create a dataset")
+    parser.add_argument(
+        "--path",
+        type=str,
+        required=True,
+        help="Path to the directory with images",
+    )
+    return parser.parse_args()
+
+#Define the function to luanch the app
+def app(dataset):
+    #Get the dataset
+    dataset = get_dataset(args.path)
+    #lunch the session
+    session = fo.launch_app()
+    # View summary info about the dataset
+
+    session.dataset = dataset
+    print(dataset)
+    session.wait()
+#Run the main function
+if __name__ == "__main__":
+    args = parse_args()
+    app(args.path)
+
+
