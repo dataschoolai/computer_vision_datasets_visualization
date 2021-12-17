d2ccef3dcfa69146e06d6ce2b749b31e93782d83
ZARIF ubuntu <uzbzarif@gmail.com>
2021-12-17 14:13:50 +0900

    pred key

--- json_dataset.py
+++ json_dataset.py
@@ -1,4 +1,5 @@
 
+from os import pread
 import fiftyone as fo
 #Import libraries for reading and writing files
 from pathlib import Path
@@ -7,18 +8,48 @@ import json
 #Import library to read command line arguments
 import argparse
 
+#Define the function create fiftyone detection format
+def get_detection_data(annotations:dict,pred=False):
+    #Create detections list for the dataset
+    detections = []
+    #read from annotations image width and height
+    width = annotations['images'][0]['width']
+    height = annotations['images'][0]['height']
+    
+    for ann in annotations['annotations']:
+        label = '103'
+        # Bounding box coordinates should be relative values
+        # in [0, 1] in the following format:
+        # [top-left-x, top-left-y, width, height]
+        #Read bounding box from annotation
+        bbox = ann['bbox']
+        #Get confidence from annotation
+        confidence = ann.get('score',False)
+        #Normalize bounding box
+        x1 = bbox[0]/width
+        y1 = bbox[1]/height
+        w = bbox[2]/width
+        h = bbox[3]/height
+    
+        #Create detection object
+        if pred:
+            detection = fo.Detection(label=label,bounding_box=[x1,y1,w,h],confidence=confidence)
+        else:
+            detection = fo.Detection(label=label,bounding_box=[x1,y1,w,h])
+        #Append detection to detections list
+        detections.append(detection)
+
+    return detections
 
 #Define the function create custom dataset format
-def get_sample_data(data_path:dict,pred=False):
-
-    images_path = Path(data_path[1]['path_labels'])/'images'
-    labels_path = Path(data_path[1]['path_labels'])/'labels'
-    print(data_path)
+def get_sample_data(images_path:Path,labels_path:list):
+    label_path = Path(labels_path[0]['path_labels'])/'labels'
+    pred = labels_path[0]['pred']
     #Create samples list for the dataset
     samples = []
     for path in images_path.glob('*.jpg'):
         #Read labels from file than assign to annotations
-        label =(labels_path / path.stem )
+        label =(label_path / path.stem )
         #Add extension to label
         label = label.with_suffix('.json')
         #Check if file is not exists
@@ -36,35 +67,10 @@ def get_sample_data(data_path:dict,pred=False):
 
         #Create a sample from the file path
         sample = fo.Sample(filepath=path)
-        #Convert detecyions to FiftyOne format
-        detections = []
-        #read from annotations image width and height
-        width = annotations['images'][0]['width']
-        height = annotations['images'][0]['height']
-        
-        for ann in annotations['annotations']:
-            label = '103'
-            # Bounding box coordinates should be relative values
-            # in [0, 1] in the following format:
-            # [top-left-x, top-left-y, width, height]
-            #Read bounding box from annotation
-            bbox = ann['bbox']
-            #Get confidence from annotation
-            confidence = ann.get('score',False)
-            #Normalize bounding box
-            x1 = bbox[0]/width
-            y1 = bbox[1]/height
-            w = bbox[2]/width
-            h = bbox[3]/height
-        
-            #Create detection object
-            if pred:
-                detection = fo.Detection(label=label,bounding_box=[x1,y1,w,h],confidence=confidence)
-            else:
-                detection = fo.Detection(label=label,bounding_box=[x1,y1,w,h])
-            #Append detection to detections list
-            detections.append(detection)
+        #Loop through labels
 
+        #Convert detecyions to FiftyOne format
+        detections = get_detection_data(annotations,pred)
         #Assign detections to sample
         if pred:
             sample['predictions'] = fo.Detections(detections=detections)
@@ -82,7 +88,10 @@ def app(data_path:dict):
     #Create dataset object
     dataset = fo.Dataset('Custom-data')
     #Define the images path
-    dataset.add_samples(get_sample_data(data_path))
+    images_path = Path(data_path[1]['path_labels'])/'images'
+    # labels_path = Path(data_path[1]['path_labels'])/'labels'
+
+    dataset.add_samples(get_sample_data(images_path=images_path,labels_path=data_path))
 
 
     #Launch the session 
@@ -99,11 +108,11 @@ if __name__ == '__main__':
 
     data =[{
         'path_labels':'/media/buntuml/DATASET/dataschoolai/yolov5_inference/dataset_json',
-        'type':'predictions'
+        'pred': True
     },
     {
         'path_labels':'/media/buntuml/DATASET/dataschoolai/yolo_converters/DATASET_JSON',
-        'type':'ground_truth'
+        'pred':False
     }
     ]
     #List of paths to the data
